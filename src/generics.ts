0024da7d4365e740e2ff54c5e69582d6f95cb9e7
Shuaiyin <shuaiyin.top@foxmail.com>
2021-06-16 07:48:31 +0800

    泛型约束

--- src/generics.ts
+++ src/generics.ts
@@ -1,17 +1,43 @@
-function log<T>(value: T): T {
-    console.log(value);
-    return value;
-}
+// function log<T>(value: T): T {
+//     console.log(value);
+//     return value;
+// }
 
-log<string[]>(['a', 'b']);
-log(['a', 'b']);
+// log<string[]>(['a', 'b']);
+// log(['a', 'b']);
 
 // 泛型函数
 // type Log = <T>(value: T) => T;
 // let myLog: Log = log
 
 // 泛型接口
-interface Log<T> {
-    (value: T): T
+// interface Log<T> {
+//     (value: T): T
+// }
+// let myLog: Log<number> = log;
+
+class Log<T> {
+    run(value: T) {
+        console.log(value);
+        return value;
+    }
 }
-let myLog: Log<number> = log;
+
+let log1 = new Log<number>();
+log1.run(1);
+let log2 = new Log();
+log2.run('1');
+
+// 泛型约束
+interface Length {
+    length: number;
+}
+
+function log<T extends Length>(value: T): T {
+    console.log(value, value.length);
+    return value;
+}
+
+// log([1]);
+// log('123');
+// log({length: 1});
